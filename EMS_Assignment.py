56b1feed4bdb998b3d252ee60b343b9ce654c499
vibkvi <beekay.vv@gmail.com>
2025-05-26 12:51:54 +0530

    Update EMS_Assignment.py
    updating conditions

--- EMS_Assignment.py
+++ EMS_Assignment.py
@@ -1,27 +1,36 @@
-def printall(emplist):
-    for empid in emplist.keys():
-        printemp(empid, emplist.get(empid))
+def printheader(id="Id", name="Name", age="Age", dept="Department", sal="Salary"):
+    print("%10s\t%20s\t%4s\t%20s\t%9s" %(id,name,age,dept,sal))
 
-def printemp(empid, emp):
-    print("------------------------------------")
-    print("Id: ", empid)
-    for key in emp.keys():
-        print(key, ": ", emp.get(key))
+def printemp(id, emp):
+    printheader(id, emp.get("name"), emp.get("age"), emp.get("dept"), emp.get("sal"))
+
+def printall(emplist):
+    if emplist.keys().__len__() == 0 :
+        print("No employees available.")
+    else:
+        printheader()
+        for empid in emplist.keys():
+            printemp(empid, emplist.get(empid))
 
 def printselectedemp(emplist) :
     key = int(input("Employee Id: "))
+    printheader()
     if key in emplist.keys():
         printemp(key, emplist.get(key))
     else:
-        print("There is no employee with id", key)
+        print("Employee not found.")
 
 def createemployee(name, age, dept, salary):
-    emp =  {'name': name, 'age': age, 'department': dept, 'salary': salary}
+    emp =  {'name': name, 'age': age, 'dept': dept, 'sal': salary}
     return emp
 
 def getemployee(emplist):
     print("Enter employee details")
     empid = int(input("Number: "))
+    while(empid in emplist.keys()):
+        print("Employee id ", empid, " already exists. Please enter a new id")
+        empid = int(input())
+
     name = input("Name: ")
     age = int(input("Age: "))
     dept = input("Department: ")
@@ -38,7 +47,7 @@ def getchoice():
     return ch
 
 choice = 0
-emps = {101: {'name': 'Satya', 'age': 27, 'department': 'HR', 'salary': 50000}}
+emps = dict()
 while choice < 4 :
     choice = getchoice()
     match choice :
